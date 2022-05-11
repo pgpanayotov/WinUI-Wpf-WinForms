817e4da83f292c49a5e33025a463dac3111a79c5
plame <plame@PLAMEN-PC>
2022-05-11 17:36:00 +0300

    Add project files.

---
+++ EmployeeManager.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManager.DataAccess;
+using EmployeeManager.ViewModel;
+using System.Windows;
+
+namespace EmployeeManager.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainViewModel viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            viewModel = new MainViewModel(new EmployeeDataProvider());
+            DataContext = viewModel;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            viewModel.Load();
+        }
+    }
+}
